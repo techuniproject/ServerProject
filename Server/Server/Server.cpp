b1bf04973dac00a41c02717f2bb49532bcd9115c
royjwseo <royjwseo@gmail.com>
2025-07-26 01:17:14 +0900

    feat : Update filter

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -10,16 +10,44 @@ using namespace std;
 #include "ThreadManager.h"
 #include "SocketUtils.h"
 
+// Select 모델 = (select 함수)
+
+// socket set
+// 1) 읽기[] 쓰기[] 예외[] 관찰 대상
+// 2) select (readSet, writeSet, exceptSet ) -> 관찰 시작
+// 3) 적어도 하나의 소켓 준비되면 리턴 -> 낙오자는 알아서 제거
+// 4) 남은 소켓 체크해서 진행
+
+// windows - select()의 한계
+// 1. FD_SETSIZE 제한 - Windows에서는 기본적으로 64개의 소켓만 감시 가능(소켓 수 제한은 대규모 서버에선 치명적)
+// 2. 선형 탐색 방식 - select()는 소켓을 선형으로 다 뒤지며 상태 확인 O(N) 비용이 매 호출 발생
+// 3. 레벨 트리거 방식 - 이벤트가 계속 유지되면 계속 FD_ISSET()에 걸림 : 반복 처리 필요, 상태 기반이라 비효율
+// 4. 모든 감시 집합을 매번 다시 세팅 : FD_SET은 입출력 감시마다 초기화 및 세팅 해야함
+
+// IOCP - 진짜 비동기, 커널 통지 기반, 많은 스레드 감시 효율, 높은 확장성, O(1), 우선순위 스레드 큐 지우너 
+
+const int32 BUF_SIZE = 1000;
+/*
+int select(
+	int nfds,               // 사용하지 않음 (Linux용), Windows에서는 무시됨
+	fd_set* readfds,        // 읽기 감시 대상
+	fd_set* writefds,       // 쓰기 감시 대상
+	fd_set* exceptfds,      // 예외 감시 대상
+	const timeval* timeout  // 타임아웃 (NULL이면 무한 대기)
+);
+*/
+
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUF_SIZE] = {};
+	int32 recvBytes = 0;
+};
 
 
 int main()
 {
 	SocketUtils::Init();
-	//// 1) 소켓 생성
-	//WSADATA wsaData;
-	//if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	//	return 0;
-	
 
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (listenSocket == INVALID_SOCKET)
@@ -36,54 +64,47 @@ int main()
 		return 0;
 
 	if (SocketUtils::Listen(listenSocket) == false)
-		return 0;//클라는 socket열고 connect인데, 서버 listen전 connect하면 클라 소켓 오류
+		return 0;
 
-	//클라가 connect 하고 서버가 accept나중에하면, 클라 커널이 3-way 핸드셰이크 후 커널의 완료 큐에서 대기
-	// 서버가 먼저 accept 하고 클라가 connect안하면 커널의 완료 큐가 비었기 때문에 block 상태
+	fd_set reads;
+	fd_set writes;
+	//fd_set은 소켓(SOCKET)의 집합을 표현하는 구조체 (소켓 수, 소켓배열)
+	vector<Session>sessions;
+	sessions.reserve(100);
+
 
 	while (true) {
-		//Non-Blocking 소켓의 비동기 accept 방식 (비동기: 함수 호출 후 곧바로 리턴, 완료는 나중/기다리지 않고 처리 후 완성을 나중에)
-		SOCKADDR_IN clientAddr;
-		int32 addrLen = sizeof(clientAddr);
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		//여기선 원래 클라가 연결하길 기다리지만, 연결이 없으면 accept()는 INVALID_SOCKET을 리턴
-		if (clientSocket == INVALID_SOCKET) {
-			//원래 블로킹이 되어야 하는데, 논블로킹으로 설정했잖아
-			// WSAGetLastError호출하면 WSAEWOULDBLOCK 오류 코드 나옴 이상황에서, 지금은 연결이 없으니 나중에 다시 시도하라는 의미.
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue; //현재 클라가 연결을 안했으면 이 오류가 뜸 논블로킹 소켓일 경우
 		
-		// 하지만 이 방식도 continue로 계속 cpu 타임슬라이스 동안 할당받아 수행하므로, 완전한 비동기는 아니고
-	    // 타임슬라이스 끝날 때까지 계속 루프 / 효율 안좋음 / 스레드 스케줄링 계속 ready 상태
-		// 블록 소켓은 블록한번되면 입출력 처리가 되기전까진 cpu할당 못받으니 절대적으로 cpu할당 계속 받는 
-		// 논블로킹 루프형이 컨텍스트 스위칭 횟수가 많음
-		}
-		
-		cout << "Client Connected!" << endl;
+		// 소켓 셋 초기화
+		FD_ZERO(&reads);
+		//map<int,int>m; m.clear 과 유사 
 
-		//Recv
-		while (true) {
+		//listen소켓을 집합에 넣어 해당 소켓을 감시하도록 등록 (관찰대상으로)
+		FD_SET(listenSocket, &reads);
 
-			char recvBuffer[100];
-			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0); //최대 recvBuffer만큼 받을수있는
-			//recvLen은 얼마만큼 커널 recvBuf에서 읽어왔는지
+		// 소켓 등록
+		for (Session& s : sessions)
+			FD_SET(s.socket, &reads);
+
+		//[옵션] 마지막 timeout인지 설정 가능
+		int32 retVal=::select(0, &reads, nullptr, nullptr, nullptr);
+		if (retVal == SOCKET_ERROR)
+			break;
+		// select는 reads에 있는 소켓을 모두 검사, 읽을 수 있는 소켓만 reads에 남기고 나머지는 제거됨
+
+		if (FD_ISSET(listenSocket, &reads)) 
+		{ //listenSocket이 reads에 남아있는지 묻는 함수
+		 // listenSocket에서 read는 사실상 accept이므로, 이게 남아있다면 
+		 //
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
 			
-			if (recvLen == SOCKET_ERROR) {
-				
-				if(::WSAGetLastError()==WSAEWOULDBLOCK)
-				continue;
-				//뭔가 잘못되어 -1 반환되었으면 마찬가지로 원랜 블록이지만
-				//아직 상대가 send를 하지 않아 대기하는게 아닌 그냥 넘어가게함
-				
-				//TODO
-				break;
-			}
-			cout << "Recv Data : " << recvBuffer << endl;
-			cout << "Recv Len : " << recvLen << endl;
 		}
+
+
 	}
 
 	SocketUtils::Clear();
-	//::closesocket(listenSocket);
-	//::WSACleanup(); //WSAStartUp 호출 시 호출해야 하는 함수
 }
--- Server/ServerCore/03_2.non-block_socket_server.cpp
+++ Server/ServerCore/03_2.non-block_socket_server.cpp
@@ -299,3 +299,94 @@ int main()
 }
 // Non-Blocking socket 설명 전  (강의기준)
 //======================================================================================================
+
+
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <vector>
+using namespace std;
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include "TestMain.h"
+#include "ThreadManager.h"
+#include "SocketUtils.h"
+
+
+
+int main()
+{
+	SocketUtils::Init();
+	//// 1) 소켓 생성
+	//WSADATA wsaData;
+	//if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	//	return 0;
+
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
+
+	//논 블로킹
+	u_long on = 1;
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
+
+	SocketUtils::SetReuseAddress(listenSocket, true);
+
+	if (SocketUtils::BindAnyAddress(listenSocket, 7777) == false)
+		return 0;
+
+	if (SocketUtils::Listen(listenSocket) == false)
+		return 0;//클라는 socket열고 connect인데, 서버 listen전 connect하면 클라 소켓 오류
+
+	//클라가 connect 하고 서버가 accept나중에하면, 클라 커널이 3-way 핸드셰이크 후 커널의 완료 큐에서 대기
+	// 서버가 먼저 accept 하고 클라가 connect안하면 커널의 완료 큐가 비었기 때문에 block 상태
+
+	while (true) {
+		//Non-Blocking 소켓의 비동기 accept 방식 (비동기: 함수 호출 후 곧바로 리턴, 완료는 나중/기다리지 않고 처리 후 완성을 나중에)
+		SOCKADDR_IN clientAddr;
+		int32 addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		//여기선 원래 클라가 연결하길 기다리지만, 연결이 없으면 accept()는 INVALID_SOCKET을 리턴
+		if (clientSocket == INVALID_SOCKET) {
+			//원래 블로킹이 되어야 하는데, 논블로킹으로 설정했잖아
+			// WSAGetLastError호출하면 WSAEWOULDBLOCK 오류 코드 나옴 이상황에서, 지금은 연결이 없으니 나중에 다시 시도하라는 의미.
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue; //현재 클라가 연결을 안했으면 이 오류가 뜸 논블로킹 소켓일 경우
+
+			// 하지만 이 방식도 continue로 계속 cpu 타임슬라이스 동안 할당받아 수행하므로, 완전한 비동기는 아니고
+			// 타임슬라이스 끝날 때까지 계속 루프 / 효율 안좋음 / 스레드 스케줄링 계속 ready 상태
+			// 블록 소켓은 블록한번되면 입출력 처리가 되기전까진 cpu할당 못받으니 절대적으로 cpu할당 계속 받는 
+			// 논블로킹 루프형이 컨텍스트 스위칭 횟수가 많음
+		}
+
+		cout << "Client Connected!" << endl;
+
+		//Recv
+		while (true) {
+
+			char recvBuffer[100];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0); //최대 recvBuffer만큼 받을수있는
+			//recvLen은 얼마만큼 커널 recvBuf에서 읽어왔는지
+
+			if (recvLen == SOCKET_ERROR) {
+
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+				//뭔가 잘못되어 -1 반환되었으면 마찬가지로 원랜 블록이지만
+				//아직 상대가 send를 하지 않아 대기하는게 아닌 그냥 넘어가게함
+
+				//TODO
+				break;
+			}
+			cout << "Recv Data : " << recvBuffer << endl;
+			cout << "Recv Len : " << recvLen << endl;
+		}
+	}
+
+	SocketUtils::Clear();
+	//::closesocket(listenSocket);
+	//::WSACleanup(); //WSAStartUp 호출 시 호출해야 하는 함수
+}
